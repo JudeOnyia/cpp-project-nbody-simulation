304eaf722cd5d7c0f0693bf084ad0fe4fdced660
Jude Onyia <judeonyia10@gmail.com>
2020-07-15 02:06:32 -0700

    1) Wrote the 2-D vector class for convenient computation in 2-D space 2) Wrote constructors and assignment operators for the body class 3) Wrote and tested a function to calculate the individual force that    a body has on another 4) Wrote and tested a function that calculates the oriented distance    between 2 bodies 5) Wrote and tested a function that calculates the total net force    on each body from all others in an efficient method that avoids    redundant computations

--- n_body.cpp
+++ n_body.cpp
@@ -6,6 +6,8 @@ int main(){
 	using std::endl;
 	using type_t = long double;
 	using vec = typename n_body::vector<type_t>;
+	using body = typename n_body::body<type_t>;
+
 	vec objA;
 	vec objB(8.9999,3.55);
 	vec objC(3.7E39, 2.56E-12);
@@ -18,11 +20,43 @@ int main(){
 	objE+=objD;
 	objC-=objB;
 	objE*=objD;
-	objAA = objE+objD;
-	objAB = objE-objD;
-	objAC = objC*objB;
+	vec objAA = objE+objD;
+	vec objAB = objE-objD;
+	vec objAC = objC*objB;
 	cout<<(objC.norm())<<endl;
+	vec objAD = objAA * type_t(23.78);
+	vec objAE = type_t(23.78) * objAA;
+	cout<<(objAA.x)<<" "<<(objAA.y)<<endl;
+	cout<<(objAD.x)<<" "<<(objAD.y)<<endl;
+	cout<<(objAE.x)<<" "<<(objAE.y)<<endl;
 
+	type_t mA(100); vec pA(3,4); vec vA(12,-15); vec aA(-1,0.88);
+	body bdA(mA,pA,vA,aA);
+	type_t mB(400); vec pB(32.5,-14); vec vB(20,3.11); vec aB(40,57);
+	body bdB(mB,pB,vB,aB);
+	vec fab = bdA.individual_force(bdB);
+	vec dist = bdA.distance(bdB);
+	cout<<"fab: ("<<(fab.x)<<","<<(fab.y)<<")"<<endl;
+	cout<<"dist: ("<<(dist.x)<<","<<(dist.y)<<")"<<endl;
+
+	body* bd = new body[3];
+	bd[0] = bdA;
+	bd[1] = bdB;
+	type_t mC(713.592); vec pC(-11,54); vec vC(12,-15); vec aC(-10,0.88);
+	bd[2] = body(mC,pC,vC,aC);
+	n_body::work_in_time_step(bd,3);
+	vec fa_sum = bd[0].individual_force(bd[1]);
+	fa_sum += bd[0].individual_force(bd[2]);
+	cout<<"fa_sum: ("<<(fa_sum.x)<<","<<(fa_sum.y)<<")"<<endl;
+	vec fb_sum = bd[1].individual_force(bd[0]);
+	fb_sum += bd[1].individual_force(bd[2]);
+	cout<<"fb_sum: ("<<(fb_sum.x)<<","<<(fb_sum.y)<<")"<<endl;
+	vec fc_sum = bd[2].individual_force(bd[0]);
+	fc_sum += bd[2].individual_force(bd[1]);
+	cout<<"fc_sum: ("<<(fc_sum.x)<<","<<(fc_sum.y)<<")"<<endl;
+
+
+	delete[] bd;
 
 
 
--- n_body.hpp
+++ n_body.hpp
@@ -1,44 +1,119 @@
 #ifndef NBODYHPP
 #define NBODYHPP
+#include<cstddef>
 #include<cmath>
+#include<iostream>
 
 namespace n_body{
 
+	// convenient x-y component class
 	template<class T>
 	struct vector{
-		vector() : x_(0), y_(0) {} // Default constructor
-		vector(T x, T y) : x_(x), y_(y) {} // Constructor with input parameters
-		vector(vector&& other) : x_(other.x_), y_(other.y_) {} // Move constructor
-		vector& operator=(vector&& other) : x_(other.x_), y_(other.y_) { return *this; } // Move assignment
-		vector(const vector& other) : x_(other.x_), y_(other.y_) {} // Copy constructor
-		vector& operator=(const vector& other) : x_(other.x_), y_(other.y_) { return *this;} // Copy assignment
+		vector() : x(0), y(0) {} // Default constructor
+		vector(T inx, T iny) : x(inx), y(iny) {} // Constructor with input parameters
+		vector(vector&& other) : x(other.x), y(other.y) {} // Move constructor
+		vector& operator=(vector&& other) { x=other.x; y=other.y; return *this; } // Move assignment
+		vector(const vector& other) : x(other.x), y(other.y) {} // Copy constructor
+		vector& operator=(const vector& other) { x=other.x; y=other.y; return *this; } // Copy assignment
 		
 		// Operator for compound addition (+=) and binary addition
-		vector& operator+=(const vector& obj){ x_ += obj.x_; y_ += obj.y_; return *this; }
-		vector operator+(const vector& obj){ return vector((x_ + obj.x_),(y_ + obj.y_)); }
+		vector& operator+=(const vector& obj){ x += obj.x; y += obj.y; return *this; }
+		vector operator+(const vector& obj) const { return vector((x + obj.x),(y + obj.y)); }
 
 		// Operator for compound subtraction (-=) and binary subtraction
-		vector& operator-=(const vector& obj){ x_ -= obj.x_; y_ -= obj.y_; return *this; }
-		vector operator-(const vector& obj){ return vector((x_ - obj.x_),(y_ - obj.y_)); }
+		vector& operator-=(const vector& obj){ x -= obj.x; y -= obj.y; return *this; }
+		vector operator-(const vector& obj) const { return vector((x - obj.x),(y - obj.y)); }
 
 		// Operator for compound multiplication (*=) and binary multiplication
-		vector& operator*=(const vector& obj){ x_ *= obj.x_; y_ *= obj.y_; return *this; }
-		vector operator*(const vector& obj){ return vector((x_ * obj.x_),(y_ * obj.y_)); }
+		vector& operator*=(const vector& obj){ x *= obj.x; y *= obj.y; return *this; }
+		vector operator*(const vector& obj) const { return vector((x * obj.x),(y * obj.y)); }
 
-		bool operator!() const { return (x_==T(0) && y_==T(0)); } // Operator to check if vector is zero vector
+		bool operator!() const { return (x==T(0) && y==T(0)); } // Operator to check if vector is zero vector
 
-		bool operator==(const vector& obj) const { return(x_==obj.x_ && y_==obj.y_); } // Operator to check equality
+		bool operator==(const vector& obj) const { return(x==obj.x && y==obj.y); } // Operator to check equality
 
-		bool operator!=(const vector& obj) const { return(x_!=obj.x_ || y_!=obj.y_); } // Operator to check inequality
+		bool operator!=(const vector& obj) const { return(x!=obj.x || y!=obj.y); } // Operator to check inequality
 
-		T norm() const { return std::sqrt(x_*x_, y_*y_); }
+		T norm() const { return std::sqrt(x*x + y*y); }
 
-		
-
-		T x_;
-		T y_;
+		T x;
+		T y;
 	};
 
+	// Non-member class for multipling vector with constants
+	template<class T>
+	vector<T> operator*(const T& num,const vector<T>& obj) { return vector<T>((obj.x * num),(obj.y * num)); }
+	template<class T>
+	vector<T> operator*(const vector<T>& obj,const T& num) { return vector<T>((obj.x * num),(obj.y * num)); }
+
+	// class for representing celestial bodies
+	template<class T>
+	struct body{
+		
+			using vec = vector<T>;
+			const T G = T(6.67408E-11); // Gravitational constant
+
+			// Default constructor
+			body() : mass(0),position(0,0),velocity(0,0),acceleration(0,0) {}
+
+			// Move constructor
+			body(body&& other) : mass(other.mass),position(other.position),velocity(other.velocity),acceleration(other.acceleration) {}
+			// Move assignment
+			body& operator=(body&& other){ mass=other.mass; position=other.position; velocity=other.velocity; acceleration=other.acceleration; return *this; }
+			
+			// Copy constructor
+			body(const body& other) : mass(other.mass),position(other.position),velocity(other.velocity),acceleration(other.acceleration) {}
+			
+			// Copy assignment
+			body& operator=(const body& other) { mass=other.mass; position=other.position; velocity=other.velocity; acceleration=other.acceleration; return *this; }
+
+			// Constructor to initialize state of body
+			body(T m,vec p,vec v,vec a) : mass(m),position(p),velocity(v),acceleration(a) {}	
+			
+			// Memeber function to calculate the oriented individual force
+			// that a body (other) has on another body (*this) 
+			vec individual_force(const body& other) const {
+				vec r_dist = position - other.position;
+				T r = r_dist.norm();
+				vec force;
+				if(r != T(0)){
+					force = (T(-1)*G*mass*other.mass/(r*r*r)) * r_dist;
+				}
+				return force;
+			}
+
+			// Member function to calculate the oriented distance
+			// from a body (other) to another body (*this)
+			vec distance(const body& other) const{
+				return (position - other.position);
+			}
+		
+			T mass;
+			vec position;
+			vec velocity;
+			vec acceleration;
+	};
+
+	// Function to help calculate total forces on each body
+	// and update their motion accordingly
+	template<class T>
+	void work_in_time_step(body<T>* bd, std::size_t N){
+		using vec = vector<T>;
+		vec* reactions = new vec[N];
+		vec summed_forces;
+		vec force_i_j;
+		for(std::size_t i=0; i<N; ++i){
+			summed_forces = (reactions[i]) * T(-1);
+			for(std::size_t j=(i+1); j<N; ++j){
+				force_i_j = bd[i].individual_force(bd[j]);
+				reactions[j] += force_i_j;
+				summed_forces += force_i_j;
+			}
+			std::cout<<"summed force: ("<<(summed_forces.x)<<" "<<(summed_forces.y)<<std::endl; // Replace with update_state function
+		}
+
+		delete[] reactions;
+	}
 
 
 
